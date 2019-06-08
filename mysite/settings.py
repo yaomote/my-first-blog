e430a1580386fabca3861c05489d0bd7e8f9e45f
yaomote <susukaYaomote@gmail.com>
2019-06-08 10:54:22 +0900

    Files have been corrected

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
         'NAME': 'djangogirls',
         'USER': 'name',
         'PASSWORD': '',
